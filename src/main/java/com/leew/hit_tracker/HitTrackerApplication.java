bd1d89357d0c0691501e2c4388eb5ad0701c0431
yw824 <yongwoo25@gmail.com>
2024-05-31 14:38:19 +0900

    initial commit: close #1

---
+++ src/main/java/com/leew/hit_tracker/HitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.leew.hit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leew/hit_tracker/HitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leew.hit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
